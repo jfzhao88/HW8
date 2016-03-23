a50247c27b855790989ee90055f1d7006912c858
jfzhao88 <hzhao1203@gmail.com>
2016-03-23 17:10:00 -0400

    PetStore
    first commit

---
+++ WebApplication1/WebApplication1/Models/Pet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Pet
+    {
+        public String Name { get; set; }
+        public String Description { get; set; }
+        
+        public DateTime DateRec { get; set; }
+        public Int32 Quanity { get; set; }
+        public Decimal Price { get; set; }
+    }
+}
